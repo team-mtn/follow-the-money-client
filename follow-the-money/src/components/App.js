849e67b7b0c0ac44a383c3f44a98a81b3c5d3868
Nhu Trinh <nqtrinh87@gmail.com>
2019-07-22 14:16:07 -0700

    file structure

--- follow-the-money/src/components/App.js
+++ follow-the-money/src/components/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
+// import SERVER_URL from './constants/server';
+import Nav from './layout/Nav';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
