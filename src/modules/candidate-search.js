7396d83307f7356e5899cd54bced98d8b849a471
twardcox <t.ward.cox@gmail.com>
2019-07-23 09:46:17 -0700

    styled victory graph with theme

--- src/modules/candidate-search.js
+++ src/modules/candidate-search.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as V from 'victory';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
 
 class CandidateSearch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'coconut',
-      candidateList: ['sanders, bernie', 'clinton, hillary', 'trump, donald'],
-      candidate: { candidate_id: 'P9723466', candidate_name: 'SANDERS, BERINIE', data: [{ range: 1, earnings: 13000 }, { range: 2, earnings: 16500 }, { range: 3, earnings: 14250 }, { range: 4, earnings: 19000 }, { range: 5, earnings: 19000 }], wiki: {}, twitter: {} }
+      value: 'no one',
+      candidateList: ['', 'sanders, bernie', 'clinton, hillary', 'trump, donald'],
+      candidate: { candidate_id: 'P9723466', candidate_name: 'SANDERS, BERINIE', data: [{ range: 1, earnings: 13000 }, { range: 2, earnings: 16500 }, { range: 3, earnings: 14250 }, { range: 4, earnings: 19000 }, { range: 5, earnings: 19000 }], wiki: { data: 'whatever we want goes here' }, twitter: {} }
     };
   }
 
@@ -25,7 +25,7 @@ class CandidateSearch extends React.Component {
   render() {
     let optionList = this.state.candidateList.map(v => <option value={v}>{v}</option>);
     return (
-      <article>
+      <section>
         <section>
           <form onSubmit={this.handleSubmit}>
             <h1>Search For A Candidate By Name</h1>
@@ -36,16 +36,24 @@ class CandidateSearch extends React.Component {
             <input type="submit" value="Submit" />
           </form>
         </section>
-        <section>{this.state.wiki}</section>
-        <section style={{ width: '50%', margin: 'auto', backgroundColor: 'white', borderRadius: '100px'}}>
+
+        <section id="wiki">
+          <h2>{this.state.candidate.candidate_name}</h2>
+
+          <p>{this.state.candidate.wiki.data}</p>
+        </section>
+
+        <section style={{ width: '50%', margin: 'auto', backgroundColor: 'white', borderRadius: '100px' }}>
           <h1>Victory Tutorial</h1>
-          <VictoryChart ChartdomainPadding={10} theme={VictoryTheme.material}>
+          <VictoryChart domainPadding={10} theme={VictoryTheme.material}>
             <VictoryAxis tickValues={['0', '200', '500', '1k', '2k']} />
             <VictoryAxis dependentAxis tickFormat={x => `$${x / 1000}k`} />
-            <VictoryBar data={this.state.candidate.data} x={'range'} y={'earnings'} />
+            <VictoryStack colorScale={'red'}>
+              <VictoryBar data={this.state.candidate.data} x={'range'} y={'earnings'} />
+            </VictoryStack>
           </VictoryChart>
         </section>
-      </article>
+      </section>
     );
   }
 }
