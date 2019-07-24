444df6a0e972f013f77683d0ddb175eb91eb2ffd
twardcox <t.ward.cox@gmail.com>
2019-07-24 16:31:18 -0700

    Updates

---
+++ precache-manifest.7b53fe731d4c43ab159ed8891fbe72e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e2f1696145e5d210eb7606f1f77dd8",
+    "url": "/follow-the-money-client/index.html"
+  },
+  {
+    "revision": "cdb5eb55069e623a0d02",
+    "url": "/follow-the-money-client/static/css/main.9070d4a5.chunk.css"
+  },
+  {
+    "revision": "eaa10c68f8d2907649b0",
+    "url": "/follow-the-money-client/static/js/2.d64f9be3.chunk.js"
+  },
+  {
+    "revision": "cdb5eb55069e623a0d02",
+    "url": "/follow-the-money-client/static/js/main.fa4eb3c8.chunk.js"
+  },
+  {
+    "revision": "12d73b7d7e9667a8e9c2",
+    "url": "/follow-the-money-client/static/js/runtime~main.d09ff24a.js"
+  },
+  {
+    "revision": "69e0bf16eac0c9bba374539ba004fe2b",
+    "url": "/follow-the-money-client/static/media/navyicontop.69e0bf16.png"
+  }
+]);
