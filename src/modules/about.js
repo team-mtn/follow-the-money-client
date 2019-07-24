d87cd60652424dba72352a689ccadba27ebcdff0
twardcox <t.ward.cox@gmail.com>
2019-07-24 15:38:33 -0700

    bug fixing

--- src/modules/about.js
+++ src/modules/about.js
@@ -15,11 +15,11 @@ class About extends React.Component {
 
         <Header height={"short"} imageID={"side-logo"} page={"about"}/>
 
-        <main main className="about no-background">
+        <main className="about no-background">
           <section className="section">
             <h1>About Follow The Money</h1>
             <p>Follow the money approaches the current political climate with an eye towards how much money presidential candidates are given and by whom. Trying to create a connection and awareness between the top level candidates, their donating block, and what is happening in the news. Hopefully highlighting corporate connections and social disconnection by revealing a candidates funding base. Of course we cannot provide the names of the contributors but you may infer the income level of those that donate by the range categories provided.</p><br/>
-            <p><strong> Remember, there is a maximum that can be contributed by a single person.</strong></p><br/>
+            <p> Remember, there is a maximum that can be contributed by a single person.</p><br/>
             <p>In no way is this an exhaustive capture of all the candiates that have applied for the presidnency but is meant to give an overview of the field with some compiled current news artilcles about them.  A look is also taken at the five top funded candidates in a compartiive graph to reveal the disparity between the top two and all others. Here again we look to the range of donations to see the supporting base.</p>
           </section>
 
@@ -36,16 +36,13 @@ class About extends React.Component {
             </article>
             <article>
               <img style={{ width: '50px' }} src="../images/dog-gone.jpg" alt="I am the Walrus. Coo-Coo-Ca-Chew" />
-              <p>Nhu "I'll Cut You!" Trinh a top level graduate of the general assembly bootcamp learned how to survive in the code with twin balisongs. She ahs turned her raw talent into a formidable career cutting up code and serving it with some fava beans and a nice chianti..</p>
+              <p>Nhu "I'll Cut You!" Trinh a top level graduate of the general assembly bootcamp learned how to survive in the code with twin balisongs. She has turned her raw talent into a formidable career cutting up code and serving it with some fava beans and a nice chianti..</p>
             </article>
             <article>
               <img style={{ width: '50px' }} src="../images/i-am-the-walrus.jpg" alt="I am the Walrus. Coo-Coo-Ca-Chew" />
               <p>Travis "The Mad Man" Cox in the relentless pursuit for his totem animal finds new and imaginative ways to hunt and consume programs. In the evening you will find him in his kennel recursively gnawing a piece of code to its binary roots.</p>
             </article>
-            <article>
-              <img style={{ width: '50px' }} src="../images/i-am-the-walrus.jpg" alt="I am the Walrus. Coo-Coo-Ca-Chew" />
-              <p>Travis "The Mad Man" Cox is in the relentless pursuit for his totem animal.</p>
-            </article>
+            
           </section>
         </main>
       </React.Fragment>
